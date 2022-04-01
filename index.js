7610143ee4e51b1ecdf798fdd094c8629b719164
dailyled <49516417+dailyled@users.noreply.github.com>
2022-04-01 11:20:08 +0200

    Update index.js
    modif for error in module execution

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const https = require('https')
 const { name } = require("./package.json");
 
 module.exports = (job, settings, action, type) => {
-	if (type != "postrender") {
-		throw new Error(`Action ${name} can be only run in postrender mode, you provided: ${type}.`
-		);
-	}
+	console.log(`[${job.uid}] applying ${name} actions for ${type}...`);
 	if (!action.url) {
 		throw new Error("No url value was provided, call can't be sent.");
 	}
